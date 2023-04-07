6acb747fcab7906c8507e3ad17f2641941c37225
nandono206 <16520253@std.stei.itb.ac.id>
2023-04-08 00:34:30 +0700

    fix: error at friend

--- split_rex/lib/src/widgets/friends/friends.dart
+++ split_rex/lib/src/widgets/friends/friends.dart
@@ -336,16 +336,7 @@ class FriendPaymentInfoDetail extends ConsumerWidget {
                 Text("$accountNumber a.n. $accountName"),
               ],
             ),
-          ),
-          Container(
-              margin: const EdgeInsets.only(left: 20),
-              child: Text(name,
-                  style: const TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                      color: Color(0xFF4F4F4F))
-                    )
-          ),
+          ),   
         ],
       ),
     );
